cb3aee8fc87100566633d4170440abda84fcf094
Mauricio Rivera <mauriver21@gmail.com>
2022-12-09 22:03:40 -0500

    Impl. window factory from console.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,12 +2,6 @@
 
 use std::process::{ Command, Stdio };
 use std::io::{ BufRead, BufReader };
-use std::thread;
-use tauri::App;
-
-struct WindowOptions {
-  label: String,
-}
 
 // The payload type must implement `Serialize` and `Clone`.
 #[derive(Clone, serde::Serialize)]
@@ -18,52 +12,33 @@ struct Payload {
 //Constants.
 const TAURI_APP_PIPE: &str = "winemacappsPipe";
 
-// A window is builded with the given options.
-fn build_window(app: &App, options: WindowOptions) {
-  tauri::WindowBuilder::new(app, options.label, tauri::WindowUrl::App("/".into())).build().unwrap();
-}
-
-// Initializes a pipe to pass arguments to the running
-// program through console, for example:
-// echo "Now I can send info as expected" > $TMPDIR/winemacappspipe
-fn init_app_pipe() {
-  thread::spawn(|| {
-    let process = Command::new("/bin/bash")
-      .stdout(Stdio::piped())
-      .arg(format!("{}/{}.sh", "bin", TAURI_APP_PIPE))
-      .spawn()
-      .expect("Failed to execute command");
-    let output = process.stdout.expect("Failed to get stdout handle");
-    let reader = BufReader::new(output);
-    reader
-      .lines()
-      .filter_map(|line| line.ok())
-      .for_each(|line| { println!("====> {line}") });
-  });
-}
-
 fn main() {
-  let app = tauri::Builder
+  tauri::Builder
     ::default()
-    .build(tauri::generate_context!())
-    .expect("error while building tauri application");
-
-  let main_window_opts = WindowOptions {
-    label: "main".to_string(),
-  };
-
-  build_window(&app, main_window_opts);
-  init_app_pipe();
-
-  app.run(|_, event| {
-    match event {
-      tauri::RunEvent::ExitRequested { .. } => {
-        Command::new("/bin/bash")
-          .arg("echo 'quit' > $TMPDIR/winemacappspipe")
-          .output()
+    .setup(|app| {
+      let handle = app.handle();
+      std::thread::spawn(move || {
+        let process = Command::new("/bin/bash")
+          .stdout(Stdio::piped())
+          .arg(format!("{}/{}.sh", "bin", TAURI_APP_PIPE))
+          .spawn()
           .expect("Failed to execute command");
-      }
-      _ => {}
-    }
-  });
+        let output = process.stdout.expect("Failed to get stdout handle");
+        let reader = BufReader::new(output);
+
+        reader
+          .lines()
+          .filter_map(|line| line.ok())
+          .for_each(|line| {
+            println!("{line}");
+            tauri::WindowBuilder
+              ::new(&handle, line.to_string(), tauri::WindowUrl::App("/".into()))
+              .build()
+              .unwrap();
+          });
+      });
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("App failed");
 }
