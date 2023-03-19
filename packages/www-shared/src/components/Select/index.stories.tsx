4b1fceda27dd78ed4ec4114ba7f619f2dd12775d
Mauricio Rivera <mauriver21@gmail.com>
2023-03-19 14:06:34 -0500

    Added stories for Select and TextInput

--- packages/www-shared/src/components/Select/index.stories.tsx
+++ packages/www-shared/src/components/Select/index.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from 'storybook-solidjs';
 import { Select } from '@components';
+import { useFormHandler } from '@utils';
+import * as yup from 'yup';
 
 const meta: Meta<typeof Select> = {
   title: 'Components/Select',
@@ -10,7 +12,7 @@ type Story = StoryObj<typeof Select>;
 
 export const Overview: Story = {
   args: {
-    label: 'Countries',
+    label: 'Country',
     placeholder: 'Select a country',
   },
   render: (args) => (
@@ -23,6 +25,31 @@ export const Overview: Story = {
       ]}
     />
   ),
+};
+
+export const ControlledSelect: Story = {
+  render: () => {
+    const formHandler = useFormHandler(
+      yup.object({
+        country: yup.number().typeError('Country is required'),
+      }),
+      { silentValidation: false }
+    );
+
+    return (
+      <Select
+        formHandler={formHandler}
+        label="Country"
+        placeholder="Select a country"
+        name="country"
+        options={[
+          { value: 1, label: 'Colombia' },
+          { value: 2, label: 'Ecuador' },
+          { value: 3, label: 'Perú' },
+        ]}
+      />
+    );
+  },
 };
 
 export default meta;
---
+++ packages/www-shared/src/components/TextInput/index.stories.tsx
@@ -0,0 +1,61 @@
+import { Meta, StoryObj } from 'storybook-solidjs';
+import { Box, TextInput } from '@components';
+import { useFormHandler } from '@utils';
+import * as yup from 'yup';
+
+const meta: Meta<typeof TextInput> = {
+  title: 'Components/TextInput',
+  component: TextInput,
+  argTypes: {
+    prevSlot: {
+      options: ['None', 'Start'],
+      mapping: {
+        None: undefined,
+        Start: <Box padding={2}>Start</Box>,
+      },
+      control: { type: 'select' },
+    },
+    nextSlot: {
+      options: ['None', 'End'],
+      mapping: {
+        None: undefined,
+        End: <Box padding={2}>End</Box>,
+      },
+      control: { type: 'select' },
+    },
+  },
+};
+
+type Story = StoryObj<typeof TextInput>;
+
+export const Overview: Story = {
+  args: {
+    label: 'Name',
+    placeholder: 'Write your name',
+    helperText: 'f.e. John Wick',
+  },
+  render: (args) => <TextInput {...args} />,
+};
+
+export const ControlledInput: Story = {
+  render: (args) => {
+    const formHandler = useFormHandler(
+      yup.object({
+        name: yup.string().required(),
+      }),
+      { silentValidation: false }
+    );
+
+    return (
+      <TextInput
+        {...args}
+        formHandler={formHandler}
+        label="Name"
+        placeholder="Write your name"
+        name="name"
+      />
+    );
+  },
+};
+
+export default meta;
